01da78010c5ec75dce5c715d58717c21c6912d3f
Park-Kunbae <pgenius2@gmail.com>
2014-03-01 10:52:27 +0900

    from KB

--- app/views.py
+++ app/views.py
@@ -55,7 +55,7 @@ def login():
 def members():
     return render_template('members.html')
 
-@app.route('/mypage/')
+@app.route('/mypage/', methods=['GET','POST'])
 ###@login_required
 def myPage():
     return render_template('my_page.html')
