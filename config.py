7fd4760adb68b8df1655053ff3c07bc5b40877c6
Park-Kunbae <pgenius2@gmail.com>
2014-03-20 15:48:04 +0900

    from KB

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY = 'temp secret key'
-SQLALCHEMY_DATABASE_URI = "postgresql://arsdale:gksehrjs0710@localhost/beauty"
+SQLALCHEMY_DATABASE_URI = "postgresql://arsdale:gksehrjs0710@ramju.cafe24.com/beauty"
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
